ed8e4e4d442bff650d90ea98314cb4d4a80eb2fd
joaobr200 <joaoarantesjob@gmail.com>
2022-07-05 22:25:32 -0300

    Updates

---
+++ precache-manifest.5043a48161c80cc7e67f668436829b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cccd458ffbf64188917a22e01b61b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b8f3ec74963597a37e",
+    "url": "/static/js/2.72781a29.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.72781a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c61e111b9db5ce85ff2",
+    "url": "/static/js/main.a94ac692.chunk.js"
+  },
+  {
+    "revision": "282bba981ecf416fcbb0",
+    "url": "/static/js/runtime-main.c441b441.js"
+  },
+  {
+    "revision": "da854aa0ee84562158b6a61b8dfbe486",
+    "url": "/static/media/foodima-icon.da854aa0.svg"
+  },
+  {
+    "revision": "a2f419a254eb2bcb8c70ca005a640f13",
+    "url": "/static/media/hero.a2f419a2.jpg"
+  }
+]);
